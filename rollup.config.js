187009631ce8b83c2407c5a9c11d3c1456336193
Erez Sobel <sobelerez@gmail.com>
2018-10-23 15:49:35 +0200

    build(rollup): add back es index.js output file

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,11 @@ export default {
       file: 'dist/index.js',
       format: 'cjs',
       sourcemap: true
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true
     }
   ],
   external: ['styled-components'],
